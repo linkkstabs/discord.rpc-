bafb90b289a8155478e0e878f4d601e83ef07fe3
Mr. Procrastinator Sensei <160384195+linkkstabs@users.noreply.github.com>
2025-02-09 09:46:18 +0530

    Update rich-presence.js

--- Rich-Presence/rich-presence.js
+++ Rich-Presence/rich-presence.js
@@ -40,17 +40,17 @@ const initializeDiscordClient = (config) => {
 const updateRichPresence = (client) => {
   try {
     const richPresence = new Discord.RichPresence()
-      .setApplicationId(presenceConfig.applicationId)
-      .setType(presenceConfig.activityType)
-      .setURL(presenceConfig.url)
-      .setState(presenceConfig.state)
-      .setName(presenceConfig.name)
-      .setDetails(presenceConfig.details)
+      .setApplicationId(presenceConfig.1298676906941743197)
+      .setType(presenceConfig.WATCHING)
+      .setURL(presenceConfig.'https://www.twitch.tv/mrprocrastinatorsensei%22')
+      .setState(presenceConfig.'Serving Lord Rimuru..')
+      .setName(presenceConfig.'Forest of Jura')
+      .setDetails(presenceConfig.'Nation of Tempest')
       .setStartTimestamp(Date.now())
-      .setAssetsLargeImage(presenceConfig.largeImage)
-      .setAssetsLargeText(presenceConfig.largeImageText)
-      .setAssetsSmallImage(presenceConfig.smallImage)
-      .setAssetsSmallText(presenceConfig.smallImageText);
+      .setAssetsLargeImage(presenceConfig.'https://media.discordapp.net/attachments/1181461047736029206/1298685483546841088/tumblr_ddfd0c1510a5918a845f37d667721de6_0975f652_400.webp?ex=67a8d89b&is=67a7871b&hm=d9bb4666cc2612acd30c24f9229a94926ce6a51b44e0fae820687800b966286e&animated=true&')
+      .setAssetsLargeText(presenceConfig.'Diablo')
+      .setAssetsSmallImage(presenceConfig.'https://media.discordapp.net/attachments/1181461047736029206/1298685367519543326/tensura-rimuru.gif?ex=67a8d880&is=67a78700&hm=e67807de6e8abab96a683efeb99c855ade36df73428807a44a76367a1daec7c2&width=800&height=450&')
+      .setAssetsSmallText(presenceConfig.'Rimuru Tempest');
 
     if (presenceConfig.buttons && presenceConfig.buttons.length > 0) {
       presenceConfig.buttons.forEach(button => {
